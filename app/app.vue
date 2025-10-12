25bfda7d9e71ac869adae0a144a80a9ca014322a
Oleksiy988998 <rekedaoleksiy@gmail.com>
2025-10-12 17:56:53 +0300

    First commit

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <section class="w-full h-screen bg-[url('/bg.svg')] flex flex-col items-center justify-around">
+      <img src="/Group 561.svg" width="464px" height="96px">
+
+      <div class="mt-2 w-[374px] h-16 flex items-center justify-center bg-[#00000080] rounded-full" >
+        <p class="text-white text-lg">Lorem Ipsum is simply dummy</p>
+      </div>
+
+      <p class="text-white text-9xl text-center leading-[134.5px] drop-shadow-xl font-extrabold">Lorem ipsum <br> Lorem</p>
+
+      <button class="mt-2 w-[180px] h-[70px] bg-white rounded-full text-[#48924F]">Help Me</button>
+    </section>
+  </div>
+</template>
