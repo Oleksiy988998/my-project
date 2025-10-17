4df391407c3e81444f5c46c4d7cfbeaaf2cb69db
ТВОЄ_ІМ'Я <_EMAIL>
2025-10-17 23:31:24 +0300

    Finished work

--- app/app.vue
+++ app/app.vue
@@ -12,5 +12,158 @@
 
       <button class="mt-2 w-[180px] h-[70px] bg-white rounded-full text-[#48924F]">Help Me</button>
     </section>
+
+
+    <section class="flex flex-col items-center gap-6">
+
+      <p class="text-[#828282] text-7xl font-semibold mx-auto p-16">Services</p>
+
+      <div class="flex justify-around gap-6">
+        <div class="max-w-sm bg-white rounded-2xl shadow-md overflow-hidden transition">
+          <img src="/Rectangle 250.png" alt="" class="w-full h-48 object-cover">
+          <p class="text-lg font-bold text-[#0D4D14] mb-2">Lorem Ipsum</p>
+          <p class="text-[#0D4D14] text-sm">Lorem Ipsum is simply dummy text of the <br> printing and typesetting industry. Lorem <br> Ipsum has been the industry's standard <br> dummy text ever since the 1500s, when an <br> unknown printer took a galley of type and <br> scrambled</p>
+        </div>
+
+        <div class="max-w-sm bg-white rounded-2xl shadow-md overflow-hidden transition">
+          <img src="/Rectangle 250 (1).png" alt="" class="w-full h-48 object-cover">
+          <p class="text-lg font-bold text-[#0D4D14] mb-2">Lorem Ipsum</p>
+          <p class="text-[#0D4D14] text-sm">Lorem Ipsum is simply dummy text of the <br> printing and typesetting industry. Lorem <br> Ipsum has been the industry's standard <br> dummy text ever since the 1500s, when an <br> unknown printer took a galley of type and <br> scrambled</p>
+        </div>
+
+        <div class="max-w-sm bg-white rounded-2xl shadow-md overflow-hidden transition">
+          <img src="/Rectangle 250 (2).png" alt="" class="w-full h-48 object-cover">
+          <p class="text-lg font-bold text-[#0D4D14] mb-2">Lorem Ipsum</p>
+          <p class="text-[#0D4D14] text-sm">Lorem Ipsum is simply dummy text of the <br> printing and typesetting industry. Lorem <br> Ipsum has been the industry's standard <br> dummy text ever since the 1500s, when an <br> unknown printer took a galley of type and <br> scrambled</p>
+        </div>
+      </div>
+
+      <button class="mt-2 w-[180px] h-[70px] bg-slate-100 rounded-full text-[#48924F]">Help Me</button>
+
+      <p class="text-[#828282] text-7xl font-semibold mx-auto p-20">About Us</p>
+    </section>
+
+
+    <section class="w-full h-screen bg-[url('/bg2.svg')] flex flex-col items-center justify-around gap-6">
+      <div class="flex justify-around gap-6">
+        <div>
+          <p class="text-white text-lg font-medium text-center">Lorem Ipsum is simply dummy text of <br> the printing and typesetting industry. <br> Lorem Ipsum has been the industry's <br> standard dummy text ever since the <br> 1500s, when an unknown printer took a <br> galley of type and scrambled it to <br> make a type specimen book. It has <br> survived not only five centuries, but <br> also the leap into electronic <br> typesetting, remaining essentially <br> unchanged. It was popularised in the <br> 1960s with the release of Letraset <br> sheets containing Lorem Ipsum <br> passages, and more recently with <br> desktop publishing software like Aldus <br> PageMaker including versions of Lorem <br> Ipsum.</p>
+        </div>
+
+        <div>
+          <p class="text-white text-lg font-medium text-center">Lorem Ipsum is simply dummy text of <br> the printing and typesetting industry. <br> Lorem Ipsum has been the industry's <br> standard dummy text ever since the <br> 1500s, when an unknown printer took a <br> galley of type and scrambled it to <br> make a type specimen book. It has <br> survived not only five centuries, but <br> also the leap into electronic <br> typesetting, remaining essentially <br> unchanged. It was popularised in the <br> 1960s with the release of Letraset <br> sheets containing Lorem Ipsum <br> passages, and more recently with <br> desktop publishing software like Aldus <br> PageMaker including versions of Lorem <br> Ipsum.</p>
+        </div>
+
+        <div>
+          <p class="text-white text-lg font-medium text-center">Lorem Ipsum is simply dummy text of <br> the printing and typesetting industry. <br> Lorem Ipsum has been the industry's <br> standard dummy text ever since the <br> 1500s, when an unknown printer took a <br> galley of type and scrambled it to <br> make a type specimen book. It has <br> survived not only five centuries, but <br> also the leap into electronic <br> typesetting, remaining essentially <br> unchanged. It was popularised in the <br> 1960s with the release of Letraset <br> sheets containing Lorem Ipsum <br> passages, and more recently with <br> desktop publishing software like Aldus <br> PageMaker including versions of Lorem <br> Ipsum.</p>
+        </div>
+      </div>
+
+      <button class="mt-2 w-[180px] h-[70px] bg-white rounded-full text-[#48924F]">Help Me</button>
+
+    </section>
+
+
+    <section class="flex flex-col items-center gap-6">
+
+      <p class="text-[#828282] text-7xl font-semibold mx-auto p-16">Testymonials</p>
+
+      <div class="grid grid-cols-2 grid-rows-2 gap-8">
+
+        <div class="w-[577px] h-[233px] border border-[#6CB981] rounded-[40px]">
+
+          <div class="flex gap-4 items-start pt-7 pl-7">
+
+            <img src="/Ellipse 1.png" alt="">
+
+            <div>
+              <p class="text-[#6CB981] text-l font-bold">Courtney Henry</p>
+              <p class="text-[#0D4C14] text-sm ">Lorem Ipsum is simply dummy text of the printing and <br> typesetting industry. Lorem Ipsum has been the industry's <br> standard dummy text ever since the 1500s, when an <br> unknown printer took a galley of type and scrambled it to <br> make a type specimen book</p>
+              <img class="pt-4" src="/Group 30.png" alt="">
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="w-[577px] h-[233px] border border-[#6CB981] rounded-[40px]">
+
+          <div class="flex gap-4 items-start pt-7 pl-7">
+
+            <img src="/Ellipse 1.png" alt="">
+
+            <div>
+              <p class="text-[#6CB981] text-l font-bold">Courtney Henry</p>
+              <p class="text-[#0D4C14] text-sm ">Lorem Ipsum is simply dummy text of the printing and <br> typesetting industry. Lorem Ipsum has been the industry's <br> standard dummy text ever since the 1500s, when an <br> unknown printer took a galley of type and scrambled it to <br> make a type specimen book</p>
+              <img class="pt-4" src="/Group 30.png" alt="">
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="w-[577px] h-[233px] border border-[#6CB981] rounded-[40px]">
+
+          <div class="flex gap-4 items-start pt-7 pl-7">
+
+            <img src="/Ellipse 1.png" alt="">
+
+            <div>
+              <p class="text-[#6CB981] text-l font-bold">Courtney Henry</p>
+              <p class="text-[#0D4C14] text-sm ">Lorem Ipsum is simply dummy text of the printing and <br> typesetting industry. Lorem Ipsum has been the industry's <br> standard dummy text ever since the 1500s, when an <br> unknown printer took a galley of type and scrambled it to <br> make a type specimen book</p>
+              <img class="pt-4" src="/Group 30.png" alt="">
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="w-[577px] h-[233px] border border-[#6CB981] rounded-[40px]">
+
+          <div class="flex gap-4 items-start pt-7 pl-7">
+
+            <img src="/Ellipse 1.png" alt="">
+
+            <div>
+              <p class="text-[#6CB981] text-l font-bold">Courtney Henry</p>
+              <p class="text-[#0D4C14] text-sm ">Lorem Ipsum is simply dummy text of the printing and <br> typesetting industry. Lorem Ipsum has been the industry's <br> standard dummy text ever since the 1500s, when an <br> unknown printer took a galley of type and scrambled it to <br> make a type specimen book</p>
+              <img class="pt-4" src="/Group 30.png" alt="">
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+    <section class="flex flex-col items-center gap-6">
+
+      <p class="text-[#828282] text-7xl font-semibold mx-auto p-16">Contact Us</p>
+
+      <div class="flex items-center justify-around gap-20">
+
+        <div class="flex flex-col items-center gap-4">
+          <div class="w-96 h-14 bg-[#9FDEA9] rounded-[61px]"></div>
+          <div class="w-96 h-14 bg-[#9FDEA9] rounded-[61px]"></div>
+          <div class="w-96 h-14 bg-[#9FDEA9] rounded-[61px]"></div>
+          <div class="w-[168px] h-14 border-4 border-[#4DB96B] rounded-[61px]"></div>
+        </div>
+
+        <div class="flex flex-col items-center gap-8">
+          <img src="/Group 12.png" alt="">
+          <img src="/Group 13.png" alt="">
+          <img class="ml-[170px]" src="/Group 14.png" alt="">
+        </div>
+
+      </div>
+
+    </section>
+
+
+    <div class="w-full h-[145px] bg-[#324B29] mt-16"></div>
+
   </div>
 </template>
